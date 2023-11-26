33a9a154c17c36d123a9a9f29a553b12d8daf976
harshit <harshitvyas880@gmail.com>
2023-11-26 13:33:40 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:chatapp2/firebase_options.dart';
 import 'package:chatapp2/pages/forgotpassword.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
